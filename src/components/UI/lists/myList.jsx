1f0c57cc56ff74bbe7dfac67db1a93bba2808f4b
Gleb BUSHUKIN <gleb.bushukin@gmail.com>
2022-12-04 14:23:07 +0100

    fix name function mylist

--- src/components/UI/lists/myList.jsx
+++ src/components/UI/lists/myList.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import MyLi from './li/MyLi'
 import classes from './MyList.module.css'
 
-export default function myList({items, ...props}) {
+export default function MyList({items, ...props}) {
     return (
         <ul style={{listStyleType: "none", padding: 0, overflow: "auto"}} className={items[0].active?classes.list_active:classes.list_disabled}>
                 {items.map((elem, index)=>{
